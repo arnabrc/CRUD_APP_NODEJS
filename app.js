7dd95f3f9693fb1dec99ef0f02182cabd9aa210f
Arnab <arnabroychowdhury6@gmail.com>
2020-05-24 13:25:48 +0530

    Commit01

---
+++ app.js
@@ -0,0 +1,35 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const product = require('./routes/product.route'); // Imports routes for the products
+
+// initialize our express app
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+const username = 'ArnabRC';
+const password = encodeURIComponent('A_RC@kol-90');
+const urlEncodedPassword = 'A_RC%40kol-90';
+const dbName = 'productstutorial';
+
+let dev_db_url = `mongodb+srv://${username}:${encodeURIComponent(password)}@cluster0-oguj3.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+// let dev_db_url = 'mongodb+srv://' + username + ':' + urlEncodedPassword + '@cluster0-oguj3.mongodb.net/test?retryWrites=true&w=majority/' + dbName;
+
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/products', product);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
